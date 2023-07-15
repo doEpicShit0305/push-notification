457622af19066acaab3c03bd302dabae090ebcb1
Surbhi Kumari <surbhi.kumari@tarento.com>
2023-07-15 14:50:57 +0530

    adding initial code for push notification

---
+++ src/test/java/com/myprojects/smsnotification/SmsNotificationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myprojects.smsnotification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmsNotificationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
